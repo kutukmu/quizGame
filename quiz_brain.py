f23db012ba72352a6ef7b409fd2c7c037c1d3df3
Kerim <kutukmu@gmail.com>
2020-12-23 20:08:08 -0600

    'methods'

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,11 +1,24 @@
 import random
 
 class Brain:
+
     def __init__(self, bank):
         self.bank = bank
+        self.question_number = 0
     
-    def ask_question(self):
-        question = random.choice(self.bank)
+    def next_question(self):
+        ask  = self.bank[self.question_number]
+        answer  = input(f'Q{self.question_number + 1}: {ask.text} (True / False)?')
+        self.question_number += 1
+        
 
+
+    def checkAnwer(self , answer):
+        ans = self.question.answer
+        if ans == answer:
+            print('Good Job You won')
+        else:
+            print('Wrong Answer')
+    
 
 
